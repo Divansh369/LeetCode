5923bb08b44f6404cbc5eb30608190c19ac81e5a
Divansh <119617031+Divansh369@users.noreply.github.com>
2024-02-08 18:17:42 +0530

    Time: 8 ms (81.02%), Space: 14 MB (11.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
-public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> hash_map;
-        std::vector<int> sol;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (hash_map.find(complement) != hash_map.end()) {
-                sol.push_back(hash_map[complement]);
-                sol.push_back(i);
-                return sol;
-            }
-            hash_map[nums[i]] = i;
-        }
-        
-        return sol;
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numToIndex;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (const auto it = numToIndex.find(target - nums[i]);
+          it != numToIndex.cend())
+        return {it->second, i};
+      numToIndex[nums[i]] = i;
     }
+
+    throw;
+  }
 };
